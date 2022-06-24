064357583890c5312dcc7310cf0b602c96be6584
2017skebadu <samuelkebadu@gmail.com>
2022-06-24 18:03:38 -0400

    sd controller and sd interface edits

--- hw/src/sd_card/sd_interface.v
+++ hw/src/sd_card/sd_interface.v
@@ -20,17 +20,21 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module sd_top(
-    input wire      CLK_100MHZ,
-    input wire      SD_CD,
-    output wire     SD_SCK,
-    output wire     SD_CMD,
-    inout wire[3:0] SD_DAT,
-    input wire      BTNC,
-    output wire[15:0] led
-    );
-    
-wire rst = BTNC;
+module sd_interface(
+  input clk,
+  input cart_cfg_upd,
+  input switch_load,
+  input [7:0] din_BRAM,
+  output[7:0] dout_BRAM,
+  output      w_out,
+  output reg [14:0] addr_BRAM,
+  input      SD_CD,
+  output     SD_SCK,
+  output     SD_CMD,
+  inout [3:0] SD_DAT,
+  output reg [7:0] LED
+);
+
 wire spiClk;
 wire spiMiso;
 wire spiMosi;
@@ -44,15 +48,17 @@ assign SD_SCK = spiClk;
 assign spiMiso = SD_DAT[0];
 assign SD_DAT[1] = 1;
 
-reg clk_25mhz = 0;
+
 reg rd = 0;
 reg wr = 0;
-reg [7:0] din = 0;
+reg bram_wr = 0;
+assign w_out = bram_wr;
+wire [7:0] din; // this was reg
 wire [7:0] dout;
 wire byte_available;
 wire ready;
 wire ready_for_next_byte;
-reg [31:0] adr = 32'h00_00_00_00;
+//reg [31:0] adr = 32'h00_00_00_00;
 
 reg [15:0] bytes = 0;
 reg [1:0] bytes_read = 0;
@@ -64,16 +70,14 @@ parameter STATE_INIT = 0;
 parameter STATE_START = 1;
 parameter STATE_WRITE = 2;
 parameter STATE_READ = 3;
-reg [1:0] test_state = STATE_START; 
+reg [1:0] test_state = STATE_INIT; 
 
-reg [24:0] counter = 0; 
-reg [1:0] divide;
-reg loading = 0;
-assign led[15] = loading;
-assign led[14:10] = state;
-assign led[9] = ready;
-assign led[8] = test_state[0];
-assign led[7:0] = bytes[15:8];
+reg writing_time = 0;
+reg [7:0] sector = 0;
+
+assign din = din_BRAM;
+assign dout = dout_BRAM;
+
 sd_controller sdcont(.cs(spiCS), 
                      .mosi(spiMosi),
                      .miso(spiMiso),
@@ -85,89 +89,83 @@ sd_controller sdcont(.cs(spiCS),
                      .dout(dout),
                      .byte_available(byte_available),
                      .ready(ready),
-                     .address(adr), 
+                     .address({16'h0000, sector, 8'h00}), 
                      .ready_for_next_byte(ready_for_next_byte),
-                     .clk(clk_25mhz), 
+                     .clk(clk), 
                      .status(state)
 );
 
-always @(posedge clk_25mhz) begin
-        if(rst) begin
-            bytes <= 0;
-            bytes_read <= 0;
-            din <= 0;
-            wr <= 0;
-            rd <= 0;
-            test_state <= STATE_INIT; 
-        end
-        else begin
+always @(posedge clk) begin
+//        if(rst == 0) begin
+//            bytes <= 0;
+//            bytes_read <= 0;
+//            din <= 0;
+//            wr <= 0;
+//            rd <= 0;
+//            test_state <= STATE_INIT; 
+//        end
+        begin //else begin
             case (test_state)
                 STATE_INIT: begin
-                    if(ready) begin
+                    if(cart_cfg_upd) begin
                         test_state <= STATE_START;
                         wr <= 1;
-                        din <= 8'hAA;
+                        addr_BRAM <= 0;
+                        sector <= 1;
+                        LED <= 0;
                     end
                 end
                 STATE_START: begin
                     if(ready == 0) begin // during write
-                        adr <= { 7'b0000000, counter};
                         test_state <= STATE_WRITE;
+                        LED[7:0] <= sector;
                         wr <= 0;
                     end
                 end
                 STATE_WRITE: begin
                     if(ready) begin
-                        if(counter < 24'h00F000) begin
+                        if(sector < 64) begin
                             test_state <= STATE_START;
+                            sector <= sector + 1;
+                            LED[7:0] <= sector;
                             wr <= 1;
                         end 
-                        else begin
+                        else if(switch_load) begin
                             test_state <= STATE_READ;
+                            LED[7:0] <= 8'hFF;
+                            addr_BRAM <= 0;
+                            sector <= 0;
                             rd <= 1;
+                            wr <= 0;
                         end
                     end
                     else if(ready_for_next_byte) begin
-                        din <= counter[7:0];
+                        //din <= din_BRAM;
+                        addr_BRAM <= addr_BRAM + 1;
                     end
                 end
                 STATE_READ: begin // only reads the first 2 bytes, then halts the 512 byte sector read.
                     if(byte_available) begin
-                        rd <= 0; // stops new sector from being buffered into output, aka SD card set to IDLE state
-                        if(bytes_read == 0) begin // first clock cycle read the first byte in the bus
-                            bytes_read <= 1;
-                            bytes[15:8] <= dout;
-                        end
-                        else if(bytes_read == 1) begin // second clock cycle read the second byte in the bus
-                            bytes_read <= 2;
-                            bytes[7:0] <= dout;  
-                        end
+                        bram_wr <= 1;
+                        addr_BRAM <= addr_BRAM + 1;
                     // never leaves this state without reset
+                    end
+                    else if(ready) begin
+                        bram_wr <= 0;
+                        if(sector < 64) begin
+                            sector <= sector + 1;
+                        end
+                        else begin
+                            rd <= 0;
+                        end
+                    end
+                    else begin
+                        bram_wr <= 0;
                     end
                 end
             endcase
         end
     end
-
-always @(posedge CLK_100MHZ) begin
-        if(divide < 2'b11) begin
-            divide <= divide + 1'b1;
-        end
-        else begin
-            divide <= 2'b00;
-            clk_25mhz <= ~clk_25mhz;
-		end
-	end
-
-always @(posedge clk_25mhz) begin
-    if( counter < 24'hFFFFFF) begin
-        counter <= counter + 1'b1;
-    end
-    else begin
-        counter <= 24'h000000;
-        loading <= ~loading;
-    end
-end
 
 
 endmodule
